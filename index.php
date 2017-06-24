d81501a17c38af331f0a80477ee7a01ff7d765a8
kv1tek <kvitek_k@mail.ru>
2017-06-24 09:19:50 +0300

    add .gitignore

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
-<?php
-
-$a = 5;
-
-echo "test";
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<title>Bootstrap test</title>
+</head>
+<body>
+<div class="btn-group-vertical btn-group-md" data-toggle="buttons">
+    <a href="groups/groups_list.html" class="btn btn-info">Groups</a>
+    <a href="users/users_list.html" class="btn btn-info">Users</a>
+    <a href="lessons/lessons_list.html"class="btn btn-info">Lessons</a>
+    <a href="tasks/tasks_list.html"class="btn btn-info">Tasks</a>
+    <a href="results/results_list.html"class="btn btn-info">Results</a>
+</div>
+</body>
+</html>
